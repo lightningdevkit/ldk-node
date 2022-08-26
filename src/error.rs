3f25117156dfe75c286db4af7fa66593e77d9c0c
Elias Rohrer <ero@tnull.de>
2022-08-26 15:10:54 +0200

    Initial project layout and utils

---
+++ src/error.rs
@@ -0,0 +1,128 @@
+use bdk::blockchain::esplora;
+use lightning::ln::msgs;
+use lightning::util::errors;
+use lightning_invoice::payment;
+use std::fmt;
+use std::io;
+use std::time;
+
+#[derive(Debug)]
+/// An error that possibly needs to be handled by the user.
+pub enum LdkLiteError {
+	/// Returned when trying to start LdkLite while it is already running.
+	AlreadyRunning,
+	/// Returned when trying to stop LdkLite while it is not running.
+	NotRunning,
+	/// An input of the funding transaction tried spending a non-SegWit output. This should never happen, but
+	/// better safe than sorry..
+	FundingTxNonWitnessOuputSpend,
+	/// The funding transaction could not be finalized.
+	FundingTxNotFinalized,
+	/// A network connection has been closed.
+	ConnectionFailed,
+	/// Payment of the given invoice has already been intiated.
+	NonUniquePaymentHash,
+	/// A given peer info could not be parsed.
+	PeerInfoParse(&'static str),
+	/// A wrapped LDK `APIError`
+	LdkApi(errors::APIError),
+	/// A wrapped LDK `DecodeError`
+	LdkDecode(msgs::DecodeError),
+	/// A wrapped LDK `PaymentError`
+	LdkPayment(payment::PaymentError),
+	/// A wrapped LDK `SignOrCreationError`
+	LdkInvoiceCreation(lightning_invoice::SignOrCreationError),
+	/// A wrapped BDK error
+	Bdk(bdk::Error),
+	/// A wrapped `EsploraError`
+	Esplora(esplora::EsploraError),
+	/// A wrapped `Bip32` error
+	Bip32(bitcoin::util::bip32::Error),
+	/// A wrapped `std::io::Error`
+	StdIo(io::Error),
+	/// A wrapped `SystemTimeError`
+	StdTime(time::SystemTimeError),
+}
+
+impl fmt::Display for LdkLiteError {
+	fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+		match *self {
+			LdkLiteError::AlreadyRunning => write!(f, "LDKLite is already running."),
+			LdkLiteError::NotRunning => write!(f, "LDKLite is not running."),
+			LdkLiteError::FundingTxNonWitnessOuputSpend => write!(f, "an input of the funding transaction tried spending a non-SegWit output, which is insecure"),
+			LdkLiteError::FundingTxNotFinalized => write!(f, "the funding transaction could not be finalized"),
+			LdkLiteError::ConnectionFailed => write!(f, "network connection closed"),
+			LdkLiteError::NonUniquePaymentHash => write!(f, "an invoice must not get payed twice."),
+			LdkLiteError::PeerInfoParse(ref e) => write!(f, "given peer info could not be parsed: {}", e),
+			LdkLiteError::LdkDecode(ref e) => write!(f, "LDK decode error: {}", e),
+			LdkLiteError::LdkApi(ref e) => write!(f, "LDK API error: {:?}", e),
+			LdkLiteError::LdkPayment(ref e) => write!(f, "LDK payment error: {:?}", e),
+			LdkLiteError::LdkInvoiceCreation(ref e) => write!(f, "LDK invoice sign or creation error: {:?}", e),
+			LdkLiteError::Bdk(ref e) => write!(f, "BDK error: {}", e),
+			LdkLiteError::Esplora(ref e) => write!(f, "Esplora error: {}", e),
+			LdkLiteError::Bip32(ref e) => write!(f, "Bitcoin error: {}", e),
+			LdkLiteError::StdIo(ref e) => write!(f, "IO error: {}", e),
+			LdkLiteError::StdTime(ref e) => write!(f, "time error: {}", e),
+		}
+	}
+}
+
+impl From<errors::APIError> for LdkLiteError {
+	fn from(e: errors::APIError) -> Self {
+		Self::LdkApi(e)
+	}
+}
+
+impl From<msgs::DecodeError> for LdkLiteError {
+	fn from(e: msgs::DecodeError) -> Self {
+		Self::LdkDecode(e)
+	}
+}
+
+impl From<payment::PaymentError> for LdkLiteError {
+	fn from(e: payment::PaymentError) -> Self {
+		Self::LdkPayment(e)
+	}
+}
+
+impl From<lightning_invoice::SignOrCreationError> for LdkLiteError {
+	fn from(e: lightning_invoice::SignOrCreationError) -> Self {
+		Self::LdkInvoiceCreation(e)
+	}
+}
+
+impl From<bdk::Error> for LdkLiteError {
+	fn from(e: bdk::Error) -> Self {
+		Self::Bdk(e)
+	}
+}
+
+impl From<bdk::sled::Error> for LdkLiteError {
+	fn from(e: bdk::sled::Error) -> Self {
+		Self::Bdk(bdk::Error::Sled(e))
+	}
+}
+
+impl From<bitcoin::util::bip32::Error> for LdkLiteError {
+	fn from(e: bitcoin::util::bip32::Error) -> Self {
+		Self::Bip32(e)
+	}
+}
+
+impl From<io::Error> for LdkLiteError {
+	fn from(e: io::Error) -> Self {
+		Self::StdIo(e)
+	}
+}
+
+impl From<time::SystemTimeError> for LdkLiteError {
+	fn from(e: time::SystemTimeError) -> Self {
+		Self::StdTime(e)
+	}
+}
+
+impl From<esplora::EsploraError> for LdkLiteError {
+	fn from(e: esplora::EsploraError) -> Self {
+		Self::Esplora(e)
+	}
+}
---
+++ src/logger.rs
@@ -0,0 +1,112 @@
+pub(crate) use lightning::util::logger::Logger;
+use lightning::util::logger::Record;
+use lightning::util::ser::Writer;
+
+use chrono::Utc;
+
+use std::fs;
+use std::path::Path;
+
+pub(crate) struct FilesystemLogger {
+	file_path: String,
+}
+
+impl FilesystemLogger {
+	pub(crate) fn new(file_path: String) -> Self {
+		if let Some(parent_dir) = Path::new(&file_path).parent() {
+			fs::create_dir_all(parent_dir).unwrap();
+		}
+		Self { file_path }
+	}
+}
+impl Logger for FilesystemLogger {
+	fn log(&self, record: &Record) {
+		let raw_log = record.args.to_string();
+		let log = format!(
+			"{} {:<5} [{}:{}] {}\n",
+			Utc::now().format("%Y-%m-%d %H:%M:%S"),
+			record.level.to_string(),
+			record.module_path,
+			record.line,
+			raw_log
+		);
+		fs::OpenOptions::new()
+			.create(true)
+			.append(true)
+			.open(self.file_path.clone())
+			.unwrap()
+			.write_all(log.as_bytes())
+			.unwrap();
+	}
+}
+
+macro_rules! log_internal {
+	($logger: expr, $lvl:expr, $($arg:tt)+) => (
+		$logger.log(&lightning::util::logger::Record::new($lvl, format_args!($($arg)+), module_path!(), file!(), line!()))
+	);
+}
+pub(crate) use log_internal;
+
+macro_rules! log_given_level {
+	($logger: expr, $lvl:expr, $($arg:tt)+) => (
+		match $lvl {
+			#[cfg(not(any(feature = "max_level_off")))]
+			lightning::util::logger::Level::Error => log_internal!($logger, $lvl, $($arg)*),
+			#[cfg(not(any(feature = "max_level_off", feature = "max_level_error")))]
+			lightning::util::logger::Level::Warn => log_internal!($logger, $lvl, $($arg)*),
+			#[cfg(not(any(feature = "max_level_off", feature = "max_level_error", feature = "max_level_warn")))]
+			lightning::util::logger::Level::Info => log_internal!($logger, $lvl, $($arg)*),
+			#[cfg(not(any(feature = "max_level_off", feature = "max_level_error", feature = "max_level_warn", feature = "max_level_info")))]
+			lightning::util::logger::Level::Debug => log_internal!($logger, $lvl, $($arg)*),
+			#[cfg(not(any(feature = "max_level_off", feature = "max_level_error", feature = "max_level_warn", feature = "max_level_info", feature = "max_level_debug")))]
+			lightning::util::logger::Level::Trace => log_internal!($logger, $lvl, $($arg)*),
+			#[cfg(not(any(feature = "max_level_off", feature = "max_level_error", feature = "max_level_warn", feature = "max_level_info", feature = "max_level_debug", feature = "max_level_trace")))]
+			lightning::util::logger::Level::Gossip => log_internal!($logger, $lvl, $($arg)*),
+
+			#[cfg(any(feature = "max_level_off", feature = "max_level_error", feature = "max_level_warn", feature = "max_level_info", feature = "max_level_debug", feature = "max_level_trace"))]
+			_ => {
+				// The level is disabled at compile-time
+			},
+		}
+	);
+}
+pub(crate) use log_given_level;
+
+#[allow(unused_macros)]
+macro_rules! log_error {
+	($logger: expr, $($arg:tt)*) => (
+		log_given_level!($logger, lightning::util::logger::Level::Error, $($arg)*)
+	)
+}
+pub(crate) use log_error;
+
+#[allow(unused_macros)]
+macro_rules! log_warn {
+	($logger: expr, $($arg:tt)*) => (
+		log_given_level!($logger, lightning::util::logger::Level::Warn, $($arg)*)
+	)
+}
+pub(crate) use log_warn;
+
+#[allow(unused_macros)]
+macro_rules! log_info {
+	($logger: expr, $($arg:tt)*) => (
+		log_given_level!($logger, lightning::util::logger::Level::Info, $($arg)*)
+	)
+}
+pub(crate) use log_info;
+
+#[allow(unused_macros)]
+macro_rules! log_debug {
+	($logger: expr, $($arg:tt)*) => (
+		log_given_level!($logger, lightning::util::logger::Level::Debug, $($arg)*)
+	)
+}
+
+#[allow(unused_macros)]
+macro_rules! log_trace {
+	($logger: expr, $($arg:tt)*) => (
+		log_given_level!($logger, lightning::util::logger::Level::Trace, $($arg)*)
+	)
+}
+pub(crate) use log_trace;
