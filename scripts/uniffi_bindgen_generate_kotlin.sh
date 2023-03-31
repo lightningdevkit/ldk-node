46c9eff54d1c432a385a6e9f2c03f3d320cc04c1
Jurvis Tan <jurvis@squareup.com>
2023-03-30 17:31:58 -0700

    Copy .so file if building with Linux host

--- scripts/uniffi_bindgen_generate_kotlin.sh
+++ scripts/uniffi_bindgen_generate_kotlin.sh
@@ -5,6 +5,11 @@ PROJECT_DIR="ldk-node-jvm"
 PACKAGE_DIR="org/lightningdevkit/ldknode"
 UNIFFI_BINDGEN_BIN="cargo run --features=uniffi/cli --bin uniffi-bindgen"
 
+DYNAMIC_LIB_PATH="target/release/libldk_node.dylib"
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+  DYNAMIC_LIB_PATH="target/release/libldk_node.so"
+fi
+
 #rustup target add aarch64-apple-darwin
 #cargo build --target aarch64-apple-darwin || exit 1
 cargo build --release || exit 1
@@ -15,4 +20,4 @@ mkdir -p "$BINDINGS_DIR"/"$PROJECT_DIR"/lib/src/main/resources/darwin-aarch64/ |
 
 cp "$TARGET_DIR"/"$PACKAGE_DIR"/ldk_node.kt "$BINDINGS_DIR"/"$PROJECT_DIR"/lib/src/main/kotlin/"$PACKAGE_DIR"/ || exit 1
 #cp ./target/aarch64-apple-darwin/debug/libldk_node.dylib "$BINDINGS_DIR"/"$PROJECT_DIR"/lib/src/main/resources/darwin-aarch64/libldk_node.dylib || exit 1
-cp target/release/libldk_node.dylib "$BINDINGS_DIR"/"$PROJECT_DIR"/lib/src/main/resources/libldk_node.dylib || exit 1
+cp $DYNAMIC_LIB_PATH "$BINDINGS_DIR"/"$PROJECT_DIR"/lib/src/main/resources/libldk_node.dylib || exit 1
