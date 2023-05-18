b223a34df1df436b8f556434cd90e3f5bc702aab
Vladimir Shingarev <telelvis@gmail.com>
2023-05-18 11:26:41 +0200

    Test coverage: sign & verify node methods

--- src/test/functional_tests.rs
+++ src/test/functional_tests.rs
@@ -363,3 +363,17 @@ fn onchain_spend_receive() {
 	assert!(node_b.onchain_balance().unwrap().get_spendable() > 99000);
 	assert!(node_b.onchain_balance().unwrap().get_spendable() < 100000);
 }
+
+// Tests arbitrary message signing and later verification 
+#[test]
+fn sign_verify_msg() {
+	let (_, electrsd) = setup_bitcoind_and_electrsd();
+	let esplora_url = electrsd.esplora_url.as_ref().unwrap();
+	let node = Builder::from_config(random_config(esplora_url)).build();
+	
+	node.start().unwrap();
+
+	let msg = "OK computer".as_bytes();
+	let sig = node.sign_message(msg).unwrap();
+	assert!(node.verify_signature(msg, sig.as_str()));
+}
