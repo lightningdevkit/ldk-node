9ee2baa5061daa4d5b1778a42415720cacf4c642
Elias Rohrer <dev@tnull.de>
2023-06-06 10:21:31 +0200

    Update README

--- src/lib.rs
+++ src/lib.rs
@@ -33,22 +33,26 @@
 //! use std::str::FromStr;
 //!
 //! fn main() {
-//! 	let mut builder = Builder::new();
+//! 	let builder = Builder::new();
 //! 	builder.set_network(Network::Testnet);
 //! 	builder.set_esplora_server("https://blockstream.info/testnet/api".to_string());
+//! 	builder.set_gossip_source_rgs("https://rapidsync.lightningdevkit.org/testnet/snapshot".to_string());
 //!
 //! 	let node = builder.build();
+//!
 //! 	node.start().unwrap();
 //!
-//! 	let _funding_address = node.new_funding_address();
+//! 	let funding_address = node.new_funding_address();
 //!
 //! 	// .. fund address ..
-//!
-//! 	node.sync_wallets().unwrap();
 //!
 //! 	let node_id = PublicKey::from_str("NODE_ID").unwrap();
 //! 	let node_addr = NetAddress::from_str("IP_ADDR:PORT").unwrap();
 //! 	node.connect_open_channel(node_id, node_addr, 10000, None, false).unwrap();
+//!
+//! 	let event = node.wait_next_event();
+//! 	println!("EVENT: {:?}", event);
+//! 	node.event_handled();
 //!
 //! 	let invoice = Invoice::from_str("INVOICE_STR").unwrap();
 //! 	node.send_payment(&invoice).unwrap();
