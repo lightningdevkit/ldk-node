67dbc56bfb4df8d54a12a1a8287e532ab9330d8f
Elias Rohrer <dev@tnull.de>
2023-11-10 15:59:22 +0100

    Add Python CI job

--- scripts/uniffi_bindgen_generate_python.sh
+++ scripts/uniffi_bindgen_generate_python.sh
@@ -2,6 +2,14 @@
 BINDINGS_DIR="./bindings/python/src/ldk_node"
 UNIFFI_BINDGEN_BIN="cargo run --manifest-path bindings/uniffi-bindgen/Cargo.toml"
 
+if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	DYNAMIC_LIB_PATH="./target/release-smaller/libldk_node.so"
+else
+	DYNAMIC_LIB_PATH="./target/release-smaller/libldk_node.dylib"
+fi
+
 cargo build --profile release-smaller --features uniffi || exit 1
 $UNIFFI_BINDGEN_BIN generate bindings/ldk_node.udl --language python -o "$BINDINGS_DIR" || exit 1
-cp ./target/release-smaller/libldk_node.dylib "$BINDINGS_DIR"/libldk_node.dylib || exit 1
+
+mkdir -p $BINDINGS_DIR
+cp "$DYNAMIC_LIB_PATH" "$BINDINGS_DIR" || exit 1
