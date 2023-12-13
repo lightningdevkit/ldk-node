fec283e5d71c4d2f26a5d010bd99c752dfd62865
Elias Rohrer <dev@tnull.de>
2023-12-13 14:47:18 +0100

    Use CI to build Python wheels on different platforms

--- scripts/uniffi_bindgen_generate_python.sh
+++ scripts/uniffi_bindgen_generate_python.sh
@@ -3,9 +3,20 @@ BINDINGS_DIR="./bindings/python/src/ldk_node"
 UNIFFI_BINDGEN_BIN="cargo run --manifest-path bindings/uniffi-bindgen/Cargo.toml"
 
 if [[ "$OSTYPE" == "linux-gnu"* ]]; then
+	# Linux
 	DYNAMIC_LIB_PATH="./target/release-smaller/libldk_node.so"
-else
+elif [[ "$OSTYPE" == "darwin"* ]]; then
+	# macOS
 	DYNAMIC_LIB_PATH="./target/release-smaller/libldk_node.dylib"
+elif [[ "$OSTYPE" == "msys"* ]]; then
+	# Windows
+	DYNAMIC_LIB_PATH="./target/release-smaller/libldk_node.dll"
+elif [[ "$OSTYPE" == "cygwin"* ]]; then
+	# Windows
+	DYNAMIC_LIB_PATH="./target/release-smaller/libldk_node.dll"
+else
+	echo "Unsupported platform!: $OSTYPE"
+	exit -1
 fi
 
 cargo build --profile release-smaller --features uniffi || exit 1
