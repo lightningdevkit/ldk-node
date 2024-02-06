f0643ca2f95381480426e04c72f85c933cc4e9a3
Elias Rohrer <dev@tnull.de>
2024-02-06 10:42:01 +0100

    f Update for shorter `from_X` constructors

--- tests/integration_tests_vss.rs
+++ tests/integration_tests_vss.rs
@@ -11,7 +11,7 @@ fn channel_full_cycle_with_vss_store() {
 	let esplora_url = format!("http://{}", electrsd.esplora_url.as_ref().unwrap());
 	let mnemonic_a = generate_entropy_mnemonic();
 	let config_a = common::random_config();
-	let mut builder_a = Builder::from_entropy_bip39_mnemonic(mnemonic_a, None, Some(config_a));
+	let mut builder_a = Builder::from_mnemonic(mnemonic_a, None, Some(config_a));
 	builder_a.set_esplora_server(esplora_url.clone());
 	let vss_base_url = std::env::var("TEST_VSS_BASE_URL").unwrap();
 	let node_a =
@@ -21,7 +21,7 @@ fn channel_full_cycle_with_vss_store() {
 	println!("\n== Node B ==");
 	let mnemonic_b = generate_entropy_mnemonic();
 	let config_b = common::random_config();
-	let mut builder_b = Builder::from_entropy_bip39_mnemonic(mnemonic_b, None, Some(config_b));
+	let mut builder_b = Builder::from_mnemonic(mnemonic_b, None, Some(config_b));
 	builder_b.set_esplora_server(esplora_url);
 	let node_b = builder_b.build_with_vss_store(vss_base_url, "node_2_store".to_string()).unwrap();
 	node_b.start().unwrap();
