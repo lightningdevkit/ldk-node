d67a3af47d81502c9c1044673c928788d575c80e
Elias Rohrer <dev@tnull.de>
2024-06-18 12:01:42 +0200

    Add timeout for broadcasting transactions

--- src/tx_broadcaster.rs
+++ src/tx_broadcaster.rs
@@ -1,3 +1,4 @@
+use crate::config::TX_BROADCAST_TIMEOUT_SECS;
 use crate::logger::{log_bytes, log_error, log_trace, Logger};
 
 use lightning::chain::chaininterface::BroadcasterInterface;
@@ -12,6 +13,7 @@ use tokio::sync::mpsc;
 use tokio::sync::Mutex;
 
 use std::ops::Deref;
+use std::time::Duration;
 
 const BCAST_PACKAGE_QUEUE_SIZE: usize = 50;
 
@@ -38,44 +40,67 @@ where
 		let mut receiver = self.queue_receiver.lock().await;
 		while let Some(next_package) = receiver.recv().await {
 			for tx in &next_package {
-				match self.esplora_client.broadcast(tx).await {
-					Ok(()) => {
-						log_trace!(self.logger, "Successfully broadcast transaction {}", tx.txid());
-					},
-					Err(e) => match e {
-						esplora_client::Error::Reqwest(err) => {
-							if err.status() == StatusCode::from_u16(400).ok() {
-								// Ignore 400, as this just means bitcoind already knows the
-								// transaction.
-								// FIXME: We can further differentiate here based on the error
-								// message which will be available with rust-esplora-client 0.7 and
-								// later.
-							} else {
+				let timeout_fut = tokio::time::timeout(
+					Duration::from_secs(TX_BROADCAST_TIMEOUT_SECS),
+					self.esplora_client.broadcast(tx),
+				);
+				match timeout_fut.await {
+					Ok(res) => match res {
+						Ok(()) => {
+							log_trace!(
+								self.logger,
+								"Successfully broadcast transaction {}",
+								tx.txid()
+							);
+						},
+						Err(e) => match e {
+							esplora_client::Error::Reqwest(err) => {
+								if err.status() == StatusCode::from_u16(400).ok() {
+									// Ignore 400, as this just means bitcoind already knows the
+									// transaction.
+									// FIXME: We can further differentiate here based on the error
+									// message which will be available with rust-esplora-client 0.7 and
+									// later.
+								} else {
+									log_error!(
+										self.logger,
+										"Failed to broadcast due to HTTP connection error: {}",
+										err
+									);
+								}
+								log_trace!(
+									self.logger,
+									"Failed broadcast transaction bytes: {}",
+									log_bytes!(tx.encode())
+								);
+							},
+							_ => {
 								log_error!(
 									self.logger,
-									"Failed to broadcast due to HTTP connection error: {}",
-									err
+									"Failed to broadcast transaction {}: {}",
+									tx.txid(),
+									e
 								);
 								log_trace!(
 									self.logger,
 									"Failed broadcast transaction bytes: {}",
 									log_bytes!(tx.encode())
 								);
-							}
+							},
 						},
-						_ => {
-							log_error!(
-								self.logger,
-								"Failed to broadcast transaction {}: {}",
-								tx.txid(),
-								e
-							);
-							log_trace!(
-								self.logger,
-								"Failed broadcast transaction bytes: {}",
-								log_bytes!(tx.encode())
-							);
-						},
+					},
+					Err(e) => {
+						log_error!(
+							self.logger,
+							"Failed to broadcast transaction due to timeout {}: {}",
+							tx.txid(),
+							e
+						);
+						log_trace!(
+							self.logger,
+							"Failed broadcast transaction bytes: {}",
+							log_bytes!(tx.encode())
+						);
 					},
 				}
 			}
