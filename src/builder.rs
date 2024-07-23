ce87a4439d765dc0eac2104f788bbe9fe33389fc
jbesraa <jbesraa@gmail.com>
2024-07-23 15:09:18 +0300

    Add `NodeBuilder::PayjoinConfig` to allow ..
    Payjoin transactions

--- src/builder.rs
+++ src/builder.rs
@@ -100,6 +100,11 @@ struct LiquiditySourceConfig {
 	lsps2_service: Option<(SocketAddress, PublicKey, Option<String>)>,
 }
 
+#[derive(Debug, Clone)]
+struct PayjoinConfig {
+	payjoin_relay: payjoin::Url,
+}
+
 impl Default for LiquiditySourceConfig {
 	fn default() -> Self {
 		Self { lsps2_service: None }
@@ -141,6 +146,8 @@ pub enum BuildError {
 	WalletSetupFailed,
 	/// We failed to setup the logger.
 	LoggerSetupFailed,
+	/// Invalid Payjoin configuration.
+	InvalidPayjoinConfig,
 }
 
 impl fmt::Display for BuildError {
@@ -162,6 +169,10 @@ impl fmt::Display for BuildError {
 			Self::WalletSetupFailed => write!(f, "Failed to setup onchain wallet."),
 			Self::LoggerSetupFailed => write!(f, "Failed to setup the logger."),
 			Self::InvalidNodeAlias => write!(f, "Given node alias is invalid."),
+			Self::InvalidPayjoinConfig => write!(
+				f,
+				"Invalid Payjoin configuration. Make sure the provided arguments are valid URLs."
+			),
 		}
 	}
 }
@@ -182,6 +193,7 @@ pub struct NodeBuilder {
 	chain_data_source_config: Option<ChainDataSourceConfig>,
 	gossip_source_config: Option<GossipSourceConfig>,
 	liquidity_source_config: Option<LiquiditySourceConfig>,
+	payjoin_config: Option<PayjoinConfig>,
 }
 
 impl NodeBuilder {
@@ -197,12 +209,14 @@ impl NodeBuilder {
 		let chain_data_source_config = None;
 		let gossip_source_config = None;
 		let liquidity_source_config = None;
+		let payjoin_config = None;
 		Self {
 			config,
 			entropy_source_config,
 			chain_data_source_config,
 			gossip_source_config,
 			liquidity_source_config,
+			payjoin_config,
 		}
 	}
 
@@ -271,6 +285,14 @@ impl NodeBuilder {
 	pub fn set_gossip_source_rgs(&mut self, rgs_server_url: String) -> &mut Self {
 		self.gossip_source_config = Some(GossipSourceConfig::RapidGossipSync(rgs_server_url));
 		self
+	}
+
+	/// Configures the [`Node`] instance to enable payjoin transactions.
+	pub fn set_payjoin_config(&mut self, payjoin_relay: String) -> Result<&mut Self, BuildError> {
+		let payjoin_relay =
+			payjoin::Url::parse(&payjoin_relay).map_err(|_| BuildError::InvalidPayjoinConfig)?;
+		self.payjoin_config = Some(PayjoinConfig { payjoin_relay });
+		Ok(self)
 	}
 
 	/// Configures the [`Node`] instance to source its inbound liquidity from the given
@@ -480,6 +502,7 @@ impl NodeBuilder {
 			self.chain_data_source_config.as_ref(),
 			self.gossip_source_config.as_ref(),
 			self.liquidity_source_config.as_ref(),
+			self.payjoin_config.as_ref(),
 			seed_bytes,
 			logger,
 			Arc::new(vss_store),
@@ -501,6 +524,7 @@ impl NodeBuilder {
 			self.chain_data_source_config.as_ref(),
 			self.gossip_source_config.as_ref(),
 			self.liquidity_source_config.as_ref(),
+			self.payjoin_config.as_ref(),
 			seed_bytes,
 			logger,
 			kv_store,
@@ -584,6 +608,11 @@ impl ArcedNodeBuilder {
 	/// network.
 	pub fn set_gossip_source_p2p(&self) {
 		self.inner.write().unwrap().set_gossip_source_p2p();
+	}
+
+	/// Configures the [`Node`] instance to enable payjoin transactions.
+	pub fn set_payjoin_config(&self, payjoin_relay: String) -> Result<(), BuildError> {
+		self.inner.write().unwrap().set_payjoin_config(payjoin_relay).map(|_| ())
 	}
 
 	/// Configures the [`Node`] instance to source its gossip data from the given RapidGossipSync
@@ -733,8 +762,9 @@ impl ArcedNodeBuilder {
 fn build_with_store_internal(
 	config: Arc<Config>, chain_data_source_config: Option<&ChainDataSourceConfig>,
 	gossip_source_config: Option<&GossipSourceConfig>,
-	liquidity_source_config: Option<&LiquiditySourceConfig>, seed_bytes: [u8; 64],
-	logger: Arc<FilesystemLogger>, kv_store: Arc<DynStore>,
+	liquidity_source_config: Option<&LiquiditySourceConfig>,
+	payjoin_config: Option<&PayjoinConfig>, seed_bytes: [u8; 64], logger: Arc<FilesystemLogger>,
+	kv_store: Arc<DynStore>,
 ) -> Result<Node, BuildError> {
 	// Initialize the status fields.
 	let is_listening = Arc::new(AtomicBool::new(false));
