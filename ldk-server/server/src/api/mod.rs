8a9764ca7cd56fb38ecff6ea3921eb27ba76eedb
G8XSU <3442979+G8XSU@users.noreply.github.com>
2024-09-11 16:32:31 -0700

    Add Impl for CloseChannel Api

--- ldk-server/server/src/api/mod.rs
+++ ldk-server/server/src/api/mod.rs
@@ -2,6 +2,7 @@ pub(crate) mod bolt11_receive;
 pub(crate) mod bolt11_send;
 pub(crate) mod bolt12_receive;
 pub(crate) mod bolt12_send;
+pub(crate) mod close_channel;
 pub(crate) mod onchain_receive;
 pub(crate) mod onchain_send;
 pub(crate) mod open_channel;
