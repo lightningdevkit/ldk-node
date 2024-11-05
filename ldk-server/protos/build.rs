452395ce18310cdb105ab5f161eb296d0e070769
G8XSU <3442979+G8XSU@users.noreply.github.com>
2024-11-05 10:36:17 -0800

    Add proto definitions for ErrorResponse.

--- ldk-server/protos/build.rs
+++ ldk-server/protos/build.rs
@@ -14,11 +14,16 @@ fn main() {
 fn generate_protos() {
 	prost_build::Config::new()
 		.bytes(&["."])
-		.compile_protos(&["src/proto/api.proto", "src/proto/types.proto"], &["src/proto/"])
+		.compile_protos(
+			&["src/proto/api.proto", "src/proto/types.proto", "src/proto/error.proto"],
+			&["src/proto/"],
+		)
 		.expect("protobuf compilation failed");
 	println!("OUT_DIR: {}", &env::var("OUT_DIR").unwrap());
 	let from_path = Path::new(&env::var("OUT_DIR").unwrap()).join("api.rs");
 	fs::copy(from_path, "src/api.rs").unwrap();
 	let from_path = Path::new(&env::var("OUT_DIR").unwrap()).join("types.rs");
 	fs::copy(from_path, "src/types.rs").unwrap();
+	let from_path = Path::new(&env::var("OUT_DIR").unwrap()).join("error.rs");
+	fs::copy(from_path, "src/error.rs").unwrap();
 }
