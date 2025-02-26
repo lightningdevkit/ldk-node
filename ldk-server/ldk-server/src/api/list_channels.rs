a63c1da90746a3850a8d0b20900ccd2806ec5038
G8XSU <3442979+G8XSU@users.noreply.github.com>
2025-02-26 15:10:14 -0800

    Return LdkServerError from ListChannels API.

--- ldk-server/ldk-server/src/api/list_channels.rs
+++ ldk-server/ldk-server/src/api/list_channels.rs
@@ -1,3 +1,4 @@
+use crate::api::error::LdkServerError;
 use crate::service::Context;
 use crate::util::proto_adapter::channel_to_proto;
 use ldk_server_protos::api::{ListChannelsRequest, ListChannelsResponse};
@@ -6,7 +7,7 @@ pub(crate) const LIST_CHANNELS_PATH: &str = "ListChannels";
 
 pub(crate) fn handle_list_channels_request(
 	context: Context, _request: ListChannelsRequest,
-) -> Result<ListChannelsResponse, ldk_node::NodeError> {
+) -> Result<ListChannelsResponse, LdkServerError> {
 	let channels = context.node.list_channels().into_iter().map(|c| channel_to_proto(c)).collect();
 
 	let response = ListChannelsResponse { channels };
