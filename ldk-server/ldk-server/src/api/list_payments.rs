68ecb297d2a98db997dba284ff1448aacddb9e85
G8XSU <3442979+G8XSU@users.noreply.github.com>
2025-02-26 15:30:53 -0800

    Return LdkServerError from ListPayments API.

--- ldk-server/ldk-server/src/api/list_payments.rs
+++ ldk-server/ldk-server/src/api/list_payments.rs
@@ -1,3 +1,4 @@
+use crate::api::error::LdkServerError;
 use crate::service::Context;
 use crate::util::proto_adapter::payment_to_proto;
 use ldk_server_protos::api::{ListPaymentsRequest, ListPaymentsResponse};
@@ -6,7 +7,7 @@ pub(crate) const LIST_PAYMENTS_PATH: &str = "ListPayments";
 
 pub(crate) fn handle_list_payments_request(
 	context: Context, _request: ListPaymentsRequest,
-) -> Result<ListPaymentsResponse, ldk_node::NodeError> {
+) -> Result<ListPaymentsResponse, LdkServerError> {
 	let payments = context.node.list_payments().into_iter().map(|p| payment_to_proto(p)).collect();
 
 	let response = ListPaymentsResponse { payments };
