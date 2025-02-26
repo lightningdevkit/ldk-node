f2dc683edcb4f5078c1f536a833a931ae3929e93
G8XSU <3442979+G8XSU@users.noreply.github.com>
2025-02-26 14:46:09 -0800

    Return LdkServerError from OnchainSend API.

--- ldk-server/ldk-server/src/api/onchain_send.rs
+++ ldk-server/ldk-server/src/api/onchain_send.rs
@@ -1,3 +1,5 @@
+use crate::api::error::LdkServerError;
+use crate::api::error::LdkServerErrorCode::InvalidRequestError;
 use crate::service::Context;
 use ldk_node::bitcoin::{Address, FeeRate};
 use ldk_server_protos::api::{OnchainSendRequest, OnchainSendResponse};
@@ -7,11 +9,16 @@ pub(crate) const ONCHAIN_SEND_PATH: &str = "OnchainSend";
 
 pub(crate) fn handle_onchain_send_request(
 	context: Context, request: OnchainSendRequest,
-) -> Result<OnchainSendResponse, ldk_node::NodeError> {
+) -> Result<OnchainSendResponse, LdkServerError> {
 	let address = Address::from_str(&request.address)
 		.map_err(|_| ldk_node::NodeError::InvalidAddress)?
 		.require_network(context.node.config().network)
-		.map_err(|_| ldk_node::NodeError::InvalidAddress)?;
+		.map_err(|_| {
+			LdkServerError::new(
+				InvalidRequestError,
+				"Address is not valid for LdkServer's configured network.".to_string(),
+			)
+		})?;
 
 	let fee_rate = request.fee_rate_sat_per_vb.map(FeeRate::from_sat_per_vb).flatten();
 	let txid = match (request.amount_sats, request.send_all) {
@@ -22,7 +29,12 @@ pub(crate) fn handle_onchain_send_request(
 		(None, Some(true)) => {
 			context.node.onchain_payment().send_all_to_address(&address, false, fee_rate)?
 		},
-		_ => return Err(ldk_node::NodeError::InvalidAmount),
+		_ => {
+			return Err(LdkServerError::new(
+				InvalidRequestError,
+				"Must specify either `send_all` or `amount_sats`, but not both or neither",
+			))
+		},
 	};
 	let response = OnchainSendResponse { txid: txid.to_string() };
 	Ok(response)
