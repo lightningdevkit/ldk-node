afad40d1b074dd0c3c2c1ffe8e7c62fb634bb597
G8XSU <3442979+G8XSU@users.noreply.github.com>
2025-03-18 16:22:41 -0700

    Start publishing more payment events using EventPublisher.

--- ldk-server/ldk-server/src/io/events/mod.rs
+++ ldk-server/ldk-server/src/io/events/mod.rs
@@ -1,1 +1,13 @@
 pub(crate) mod event_publisher;
+
+use ldk_server_protos::events::event_envelope;
+
+/// Event variant to event name mapping.
+pub(crate) fn get_event_name(event: &event_envelope::Event) -> &'static str {
+	match event {
+		event_envelope::Event::PaymentReceived(_) => "PaymentReceived",
+		event_envelope::Event::PaymentSuccessful(_) => "PaymentSuccessful",
+		event_envelope::Event::PaymentFailed(_) => "PaymentFailed",
+		event_envelope::Event::PaymentForwarded(_) => "PaymentForwarded",
+	}
+}
