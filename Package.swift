91ec1b6690b1486392e3b92e419b48483598bf9d
Elias Rohrer <dev@tnull.de>
2025-04-29 09:58:30 +0200

    Bump Kotlin/Swift/Python version numbers

--- Package.swift
+++ Package.swift
@@ -3,7 +3,7 @@
 
 import PackageDescription
 
-let tag = "v0.4.2"
+let tag = "v0.5.0"
 let checksum = "95ea5307eb3a99203e39cfa21d962bfe3e879e62429e8c7cdf5292cae5dc35cc"
 let url = "https://github.com/lightningdevkit/ldk-node/releases/download/\(tag)/LDKNodeFFI.xcframework.zip"
 
