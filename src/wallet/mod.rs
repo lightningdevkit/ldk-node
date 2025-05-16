705d55f7ba20b6998bb39dc9b05ab8d8e785e71e
Elias Rohrer <dev@tnull.de>
2025-05-16 15:12:13 +0200

    Don't fail `send_all` retaining reserves for 0 channels
    Previouly, `OnchainPayment::send_all_to_address` would fail in the
    `retain_reserves` mode if the maintained reserves were below the dust
    limit. Most notably this would happen if we had no channels open at all.
    
    Here, we fix this by simply falling back to the draining case (not
    considering reserves) if the anchor reserves are below dust. We also add
    a unit test that would have caught this regression in the first place.

--- src/wallet/mod.rs
+++ src/wallet/mod.rs
@@ -355,6 +355,7 @@ where
 			let mut locked_wallet = self.inner.lock().unwrap();
 
 			// Prepare the tx_builder. We properly check the reserve requirements (again) further down.
+			const DUST_LIMIT_SATS: u64 = 546;
 			let tx_builder = match send_amount {
 				OnchainSendAmount::ExactRetainingReserve { amount_sats, .. } => {
 					let mut tx_builder = locked_wallet.build_tx();
@@ -362,7 +363,9 @@ where
 					tx_builder.add_recipient(address.script_pubkey(), amount).fee_rate(fee_rate);
 					tx_builder
 				},
-				OnchainSendAmount::AllRetainingReserve { cur_anchor_reserve_sats } => {
+				OnchainSendAmount::AllRetainingReserve { cur_anchor_reserve_sats }
+					if cur_anchor_reserve_sats > DUST_LIMIT_SATS =>
+				{
 					let change_address_info = locked_wallet.peek_address(KeychainKind::Internal, 0);
 					let balance = locked_wallet.balance();
 					let spendable_amount_sats = self
@@ -419,7 +422,8 @@ where
 						.fee_absolute(estimated_tx_fee);
 					tx_builder
 				},
-				OnchainSendAmount::AllDrainingReserve => {
+				OnchainSendAmount::AllDrainingReserve
+				| OnchainSendAmount::AllRetainingReserve { cur_anchor_reserve_sats: _ } => {
 					let mut tx_builder = locked_wallet.build_tx();
 					tx_builder.drain_wallet().drain_to(address.script_pubkey()).fee_rate(fee_rate);
 					tx_builder
