02ed4791aaec92125971c72d8831822f21d8c251
Chuks Agbakuru <chuksagbakuru@gmail.com>
2025-08-02 17:19:21 +0100

    rename UnifiedQRPayment to UnifiedPayment, rename QRPaymentResult to UnifiedPaymentResult
    These renamings are necessary to reflect the expanded responsibilities for this module.

--- src/lib.rs
+++ src/lib.rs
@@ -131,25 +131,13 @@ use gossip::GossipSource;
 use graph::NetworkGraph;
 pub use io::utils::generate_entropy_mnemonic;
 use io::utils::write_node_metrics;
-use lightning::chain::BestBlock;
-use lightning::events::bump_transaction::Wallet as LdkWallet;
-use lightning::impl_writeable_tlv_based;
-use lightning::ln::channel_state::ChannelShutdownState;
-use lightning::ln::channelmanager::PaymentId;
-use lightning::ln::msgs::SocketAddress;
-use lightning::routing::gossip::NodeAlias;
-use lightning::util::persist::KVStoreSync;
-use lightning_background_processor::process_events_async;
 use liquidity::{LSPS1Liquidity, LiquiditySource};
-use logger::{log_debug, log_error, log_info, log_trace, LdkLogger, Logger};
-use payment::asynchronous::om_mailbox::OnionMessageMailbox;
 use payment::asynchronous::static_invoice_store::StaticInvoiceStore;
 use payment::{
 	Bolt11Payment, Bolt12Payment, OnchainPayment, PaymentDetails, SpontaneousPayment,
-	UnifiedQrPayment,
+	UnifiedPayment,
 };
 use peer_store::{PeerInfo, PeerStore};
-use rand::Rng;
 use runtime::Runtime;
 use types::{
 	Broadcaster, BumpTransactionEventHandler, ChainMonitor, ChannelManager, Graph, KeysManager,
@@ -159,6 +147,18 @@ pub use types::{
 	ChannelDetails, CustomTlvRecord, DynStore, PeerDetails, SyncAndAsyncKVStore, UserChannelId,
 };
 
+use lightning::chain::BestBlock;
+use lightning::events::bump_transaction::Wallet as LdkWallet;
+use lightning::impl_writeable_tlv_based;
+use lightning::ln::channel_state::ChannelShutdownState;
+use lightning::ln::channelmanager::PaymentId;
+use lightning::ln::msgs::SocketAddress;
+use lightning::routing::gossip::NodeAlias;
+use lightning::util::persist::KVStoreSync;
+use lightning_background_processor::process_events_async;
+use logger::{log_debug, log_error, log_info, log_trace, LdkLogger, Logger};
+use payment::asynchronous::om_mailbox::OnionMessageMailbox;
+use rand::Rng;
 pub use {
 	bip39, bitcoin, lightning, lightning_invoice, lightning_liquidity, lightning_types, tokio,
 	vss_client,
@@ -927,12 +927,15 @@ impl Node {
 	/// Returns a payment handler allowing to create [BIP 21] URIs with an on-chain, [BOLT 11],
 	/// and [BOLT 12] payment options.
 	///
+	/// This handler allows you to send payments to these URIs as well as [BIP 353] HRNs.
+	///
 	/// [BOLT 11]: https://github.com/lightning/bolts/blob/master/11-payment-encoding.md
 	/// [BOLT 12]: https://github.com/lightning/bolts/blob/master/12-offer-encoding.md
 	/// [BIP 21]: https://github.com/bitcoin/bips/blob/master/bip-0021.mediawiki
+	/// [BIP 353]: https://github.com/bitcoin/bips/blob/master/bip-0353.mediawiki
 	#[cfg(not(feature = "uniffi"))]
-	pub fn unified_qr_payment(&self) -> UnifiedQrPayment {
-		UnifiedQrPayment::new(
+	pub fn unified_payment(&self) -> UnifiedPayment {
+		UnifiedPayment::new(
 			self.onchain_payment().into(),
 			self.bolt11_payment().into(),
 			self.bolt12_payment().into(),
@@ -944,12 +947,15 @@ impl Node {
 	/// Returns a payment handler allowing to create [BIP 21] URIs with an on-chain, [BOLT 11],
 	/// and [BOLT 12] payment options.
 	///
+	/// This handler allows you to send payments to these URIs as well as [BIP 353] HRNs.
+	///
 	/// [BOLT 11]: https://github.com/lightning/bolts/blob/master/11-payment-encoding.md
 	/// [BOLT 12]: https://github.com/lightning/bolts/blob/master/12-offer-encoding.md
 	/// [BIP 21]: https://github.com/bitcoin/bips/blob/master/bip-0021.mediawiki
+	/// [BIP 353]: https://github.com/bitcoin/bips/blob/master/bip-0353.mediawiki
 	#[cfg(feature = "uniffi")]
-	pub fn unified_qr_payment(&self) -> Arc<UnifiedQrPayment> {
-		Arc::new(UnifiedQrPayment::new(
+	pub fn unified_payment(&self) -> Arc<UnifiedPayment> {
+		Arc::new(UnifiedPayment::new(
 			self.onchain_payment(),
 			self.bolt11_payment(),
 			self.bolt12_payment(),
--- src/payment/unified.rs
+++ src/payment/unified.rs
@@ -45,7 +45,7 @@ struct Extras {
 /// [BOLT 11]: https://github.com/lightning/bolts/blob/master/11-payment-encoding.md
 /// [BOLT 12]: https://github.com/lightning/bolts/blob/master/12-offer-encoding.md
 /// [`Node::unified_qr_payment`]: crate::Node::unified_qr_payment
-pub struct UnifiedQrPayment {
+pub struct UnifiedPayment {
 	onchain_payment: Arc<OnchainPayment>,
 	bolt11_invoice: Arc<Bolt11Payment>,
 	bolt12_payment: Arc<Bolt12Payment>,
@@ -53,7 +53,7 @@ pub struct UnifiedQrPayment {
 	logger: Arc<Logger>,
 }
 
-impl UnifiedQrPayment {
+impl UnifiedPayment {
 	pub(crate) fn new(
 		onchain_payment: Arc<OnchainPayment>, bolt11_invoice: Arc<Bolt11Payment>,
 		bolt12_payment: Arc<Bolt12Payment>, config: Arc<Config>, logger: Arc<Logger>,
@@ -138,7 +138,7 @@ impl UnifiedQrPayment {
 	/// occurs, an `Error` is returned detailing the issue encountered.
 	///
 	/// [BIP 21]: https://github.com/bitcoin/bips/blob/master/bip-0021.mediawiki
-	pub fn send(&self, uri_str: &str) -> Result<QrPaymentResult, Error> {
+	pub fn send(&self, uri_str: &str) -> Result<UnifiedPaymentResult, Error> {
 		let uri: bip21::Uri<NetworkUnchecked, Extras> =
 			uri_str.parse().map_err(|_| Error::InvalidUri)?;
 
@@ -148,7 +148,7 @@ impl UnifiedQrPayment {
 		if let Some(offer) = uri_network_checked.extras.bolt12_offer {
 			let offer = maybe_wrap(offer);
 			match self.bolt12_payment.send(&offer, None, None) {
-				Ok(payment_id) => return Ok(QrPaymentResult::Bolt12 { payment_id }),
+				Ok(payment_id) => return Ok(UnifiedPaymentResult::Bolt12 { payment_id }),
 				Err(e) => log_error!(self.logger, "Failed to send BOLT12 offer: {:?}. This is part of a unified QR code payment. Falling back to the BOLT11 invoice.", e),
 			}
 		}
@@ -156,7 +156,7 @@ impl UnifiedQrPayment {
 		if let Some(invoice) = uri_network_checked.extras.bolt11_invoice {
 			let invoice = maybe_wrap(invoice);
 			match self.bolt11_invoice.send(&invoice, None) {
-				Ok(payment_id) => return Ok(QrPaymentResult::Bolt11 { payment_id }),
+				Ok(payment_id) => return Ok(UnifiedPaymentResult::Bolt11 { payment_id }),
 				Err(e) => log_error!(self.logger, "Failed to send BOLT11 invoice: {:?}. This is part of a unified QR code payment. Falling back to the on-chain transaction.", e),
 			}
 		}
@@ -175,7 +175,7 @@ impl UnifiedQrPayment {
 			None,
 		)?;
 
-		Ok(QrPaymentResult::Onchain { txid })
+		Ok(UnifiedPaymentResult::Onchain { txid })
 	}
 }
 
@@ -188,7 +188,7 @@ impl UnifiedQrPayment {
 /// [`PaymentId`]: lightning::ln::channelmanager::PaymentId
 /// [`Txid`]: bitcoin::hash_types::Txid
 #[derive(Debug)]
-pub enum QrPaymentResult {
+pub enum UnifiedPaymentResult {
 	/// An on-chain payment.
 	Onchain {
 		/// The transaction ID (txid) of the on-chain payment.
@@ -302,9 +302,8 @@ impl DeserializationError for Extras {
 
 #[cfg(test)]
 mod tests {
-	use super::*;
-	use crate::payment::unified::Extras;
-	use bitcoin::{Address, Network};
+	use super::{Amount, Bolt11Invoice, Extras, Offer};
+	use bitcoin::{address::NetworkUnchecked, Address, Network};
 	use std::str::FromStr;
 
 	#[test]
