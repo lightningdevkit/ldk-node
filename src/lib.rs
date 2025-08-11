0eff43b18446455425cab7967adc38660918e32e
Chuks Agbakuru <chuksagbakuru@gmail.com>
2025-08-11 16:47:39 +0100

    Add HRN resolver to Node and UnifiedPayment
    Introduce the hrn_resolver dependency into the main Node struct and
    plumb it through to the UnifiedPayment component. This is necessary to
    allow the node to resolve offers presented via a Human-Readable Name (HRN).

--- src/lib.rs
+++ src/lib.rs
@@ -159,7 +159,8 @@ use rand::Rng;
 use runtime::Runtime;
 use types::{
 	Broadcaster, BumpTransactionEventHandler, ChainMonitor, ChannelManager, DynStore, Graph,
-	KeysManager, OnionMessenger, PaymentStore, PeerManager, Router, Scorer, Sweeper, Wallet,
+	HRNResolver, KeysManager, OnionMessenger, PaymentStore, PeerManager, Router, Scorer, Sweeper,
+	Wallet,
 };
 pub use types::{ChannelDetails, CustomTlvRecord, PeerDetails, SyncAndAsyncKVStore, UserChannelId};
 pub use {
@@ -206,6 +207,7 @@ pub struct Node {
 	node_metrics: Arc<RwLock<NodeMetrics>>,
 	om_mailbox: Option<Arc<OnionMessageMailbox>>,
 	async_payments_role: Option<AsyncPaymentsRole>,
+	hrn_resolver: Arc<HRNResolver>,
 }
 
 impl Node {
@@ -959,6 +961,7 @@ impl Node {
 			self.bolt12_payment().into(),
 			Arc::clone(&self.config),
 			Arc::clone(&self.logger),
+			Arc::clone(&self.hrn_resolver),
 		)
 	}
 
@@ -979,6 +982,7 @@ impl Node {
 			self.bolt12_payment(),
 			Arc::clone(&self.config),
 			Arc::clone(&self.logger),
+			Arc::clone(&self.hrn_resolver),
 		))
 	}
 
