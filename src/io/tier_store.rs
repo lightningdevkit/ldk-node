02debf54d5843d29416c1e9529d059d7e49adb56
Enigbe <ochekliyeenigbe@gmail.com>
2026-02-16 18:36:52 +0100

    Remove unnecessary uniffi gating in tier store

--- src/io/tier_store.rs
+++ src/io/tier_store.rs
@@ -706,7 +706,6 @@ mod tests {
 	use crate::io::tier_store::TierStore;
 	use crate::logger::Logger;
 	use crate::runtime::Runtime;
-	#[cfg(not(feature = "uniffi"))]
 	use crate::types::DynStore;
 	use crate::types::DynStoreWrapper;
 
