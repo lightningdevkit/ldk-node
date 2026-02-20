1debcf8407138f0abca2b92b5ab23e48d4b56837
Matt Corallo <git@bluematt.me>
2026-02-20 17:43:20 +0000

    f drop test we can't easily upgrade

--- tests/integration_tests_vss.rs
+++ tests/integration_tests_vss.rs
@@ -13,7 +13,7 @@ use std::collections::HashMap;
 
 use ldk_node::entropy::NodeEntropy;
 use ldk_node::Builder;
-use rand::{rng, Rng};
+use rand::{rng, RngCore};
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn channel_full_cycle_with_vss_store() {
@@ -56,74 +56,6 @@ async fn channel_full_cycle_with_vss_store() {
 }
 
 #[tokio::test(flavor = "multi_thread", worker_threads = 1)]
-async fn vss_v0_schema_backwards_compatibility() {
-	let (bitcoind, electrsd) = common::setup_bitcoind_and_electrsd();
-	let esplora_url = format!("http://{}", electrsd.esplora_url.as_ref().unwrap());
-	let vss_base_url = std::env::var("TEST_VSS_BASE_URL").unwrap();
-
-	let storage_path = common::random_storage_path().to_str().unwrap().to_owned();
-	let mut seed_bytes = [42u8; 64];
-	rand::thread_rng().fill_bytes(&mut seed_bytes);
-	let node_entropy = NodeEntropy::from_seed_bytes(seed_bytes);
-
-	// Setup a v0.6.2 `Node` persisted with the v0 scheme.
-	let (old_balance, old_node_id) = {
-		let mut builder_old = ldk_node_062::Builder::new();
-		builder_old.set_network(bitcoin::Network::Regtest);
-		builder_old.set_storage_dir_path(storage_path.clone());
-		builder_old.set_entropy_seed_bytes(seed_bytes);
-		builder_old.set_chain_source_esplora(esplora_url.clone(), None);
-		let node_old = builder_old
-			.build_with_vss_store(vss_base_url.clone(), "".to_owned(), HashMap::new())
-			.unwrap();
-
-		node_old.start().unwrap();
-		let addr_old = node_old.onchain_payment().new_address().unwrap();
-		common::premine_and_distribute_funds(
-			&bitcoind.client,
-			&electrsd.client,
-			vec![addr_old],
-			bitcoin::Amount::from_sat(100_000),
-		)
-		.await;
-		node_old.sync_wallets().unwrap();
-
-		let balance = node_old.list_balances().spendable_onchain_balance_sats;
-		assert!(balance > 0);
-		let node_id = node_old.node_id();
-
-		// Workaround necessary as v0.6.2's VSS runtime wasn't dropsafe in a tokio context.
-		tokio::task::block_in_place(move || {
-			node_old.stop().unwrap();
-			drop(node_old);
-		});
-
-		(balance, node_id)
-	};
-
-	// Now ensure we can still reinit from the same backend.
-	let mut builder_new = Builder::new();
-	builder_new.set_network(bitcoin::Network::Regtest);
-	builder_new.set_storage_dir_path(storage_path);
-	builder_new.set_chain_source_esplora(esplora_url, None);
-
-	let node_new = builder_new
-		.build_with_vss_store(node_entropy, vss_base_url, "".to_owned(), HashMap::new())
-		.unwrap();
-
-	node_new.start().unwrap();
-	node_new.sync_wallets().unwrap();
-
-	let new_balance = node_new.list_balances().spendable_onchain_balance_sats;
-	let new_node_id = node_new.node_id();
-
-	assert_eq!(old_node_id, new_node_id);
-	assert_eq!(old_balance, new_balance);
-
-	node_new.stop().unwrap();
-}
-
-#[tokio::test(flavor = "multi_thread", worker_threads = 1)]
 async fn vss_node_restart() {
 	let (bitcoind, electrsd) = common::setup_bitcoind_and_electrsd();
 	let esplora_url = format!("http://{}", electrsd.esplora_url.as_ref().unwrap());
@@ -131,7 +63,7 @@ async fn vss_node_restart() {
 
 	let storage_path = common::random_storage_path().to_str().unwrap().to_owned();
 	let mut seed_bytes = [42u8; 64];
-	rand::thread_rng().fill_bytes(&mut seed_bytes);
+	rand::rng().fill_bytes(&mut seed_bytes);
 	let node_entropy = NodeEntropy::from_seed_bytes(seed_bytes);
 
 	// Setup initial node and fund it.
