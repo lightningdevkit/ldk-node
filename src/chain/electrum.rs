9eaa12da2eab4c6839955eadcc50870f3fa11acc
Leo Nash <hello@leonash.net>
2026-06-25 16:02:34 +0000

    Submit TRUC packages via all chain sources
    We rely on the `BroadcasterInterface` contract whereby any
    multi-transaction vector must be a single child and its parents, and
    must be broadcasted together as a package using `submitpackage`.
    
    In a prior commit, we added the guarantee that any packages received
    from the broadcast queue are already topologically sorted, and hence
    can be passed directly to the `submit_package` Bitcoin Core RPC.

--- src/chain/electrum.rs
+++ src/chain/electrum.rs
@@ -355,8 +355,12 @@ impl ElectrumChainSource {
 			return;
 		};
 
-		for tx in txs.into_inner() {
-			electrum_client.broadcast(tx).await;
+		match txs.len() {
+			0 => (),
+			1 => {
+				electrum_client.broadcast(txs.try_into_single_tx().expect("The length is 1")).await
+			},
+			2.. => electrum_client.submit_package(txs).await,
 		}
 	}
 }
@@ -631,6 +635,46 @@ impl ElectrumRuntimeClient {
 				Err(e) => self.log_broadcast_error(e, &[txid], tx.as_ref()),
 			},
 			Err(e) => self.log_broadcast_error(e, &[txid], tx.as_ref()),
+		}
+	}
+
+	async fn submit_package(&self, package: SortedTransactions) {
+		let electrum_client = Arc::clone(&self.electrum_client);
+
+		let txids: Vec<_> = package.iter().map(|tx| tx.compute_txid()).collect();
+		let package = Arc::new(package);
+
+		let spawn_fut = self.runtime.spawn_blocking({
+			let package = Arc::clone(&package);
+			move || electrum_client.transaction_broadcast_package(&package)
+		});
+		let timeout_fut = tokio::time::timeout(
+			Duration::from_secs(self.sync_config.timeouts_config.tx_broadcast_timeout_secs),
+			spawn_fut,
+		);
+
+		match timeout_fut.await {
+			Ok(res) => match res {
+				Ok(Ok(result)) => {
+					if result.success {
+						log_trace!(
+							self.logger,
+							"Successfully broadcast transaction(s) {:?}",
+							txids
+						);
+						log_trace!(
+							self.logger,
+							"Successfully broadcast transaction(s) {:?}",
+							result
+						);
+					} else {
+						self.log_broadcast_error(format!("{:?}", result), &txids, &package);
+					}
+				},
+				Ok(Err(e)) => self.log_broadcast_error(e, &txids, &package),
+				Err(e) => self.log_broadcast_error(e, &txids, &package),
+			},
+			Err(e) => self.log_broadcast_error(e, &txids, &package),
 		}
 	}
 
