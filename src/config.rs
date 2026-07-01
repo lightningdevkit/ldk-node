a6453c6dfa55f96adf6ea628b327451f041365ea
Leo Nash <hello@leonash.net>
2026-07-01 19:04:03 +0000

    f: drop enable_zero_fee_commitments

--- src/config.rs
+++ src/config.rs
@@ -289,11 +289,10 @@ impl Default for HumanReadableNamesConfig {
 ///
 /// ### Defaults
 ///
-/// | Parameter                     | Value  |
-/// |-------------------------------|--------|
-/// | `trusted_peers_no_reserve`    | []     |
-/// | `per_channel_reserve_sats`    | 25000  |
-/// | `enable_zero_fee_commitments` | false  |
+/// | Parameter                  | Value  |
+/// |----------------------------|--------|
+/// | `trusted_peers_no_reserve` | []     |
+/// | `per_channel_reserve_sats` | 25000  |
 ///
 ///
 /// [BOLT 3]: https://github.com/lightning/bolts/blob/master/03-transactions.md#htlc-timeout-and-htlc-success-transactions
@@ -329,12 +328,6 @@ pub struct AnchorChannelsConfig {
 	/// might not suffice to successfully spend the Anchor output and have the HTLC transactions
 	/// confirmed on-chain, i.e., you may want to adjust this value accordingly.
 	pub per_channel_reserve_sats: u64,
-	/// In addition to `option_anchors_zero_fee_htlc_tx`, we will also attempt to negotiate
-	/// `option_zero_fee_commitments`. Zero-fee commitment channels remove all commitment
-	/// feerate negotiation from the channel, and instead source *all* the fees required to
-	/// confirm the commitment from the anchor reserve at the time of broadcast.
-	/// See [BOLT 3] for more technical details.
-	pub enable_zero_fee_commitments: bool,
 }
 
 impl Default for AnchorChannelsConfig {
@@ -342,7 +335,6 @@ impl Default for AnchorChannelsConfig {
 		Self {
 			trusted_peers_no_reserve: Vec::new(),
 			per_channel_reserve_sats: DEFAULT_ANCHOR_PER_CHANNEL_RESERVE_SATS,
-			enable_zero_fee_commitments: false,
 		}
 	}
 }
